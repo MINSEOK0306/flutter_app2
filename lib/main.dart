d7aabc2797ca8cd97014f783db337ad2bb334a9b
minseuk0306@naver.com <minseuk0306@naver.com>
2026-05-07 12:04:11 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  print("hello world");
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("레이아웃 예제"),),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text("첫 번째"),
+              Text("두 번째"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(
+//           seedColor: Colors.deepPurple,
+//         ),
+//       ),
+//       home: const MyHomePage(title: '나는 데이터분석가'),
+//     );
+//   }
+// }
+//
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
+//
+//   final String title;
+//
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   // int _counter = 0;
+//   //
+//   // void _incrementCounter() {
+//   //   setState(() {
+//   //     _counter++;
+//   //   });
+//   // }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor:
+//         Theme.of(context).colorScheme.inversePrimary,
+//         title: Text(widget.title),
+//       ),
+//
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Text('나는 자연인입니다.'),
+//             Text('나는 데이터분석가입니다'),
+//             // Text('카운트: $_counter'),
+//           ],
+//         ),
+//       ),
+//
+//       // floatingActionButton: FloatingActionButton(
+//       //   onPressed: _incrementCounter,
+//       //   tooltip: 'Increment',
+//       //   child: const Icon(Icons.add),
+//       // ),
+//     );
+//   }
+// }
