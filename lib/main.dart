ed202f42284b6cd484c2e0530dc7cf6897338706
minseuk0306@naver.com <minseuk0306@naver.com>
2026-05-07 15:35:19 +0900

    260507 - 박스

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
 
                   Expanded(
                     child: Container(
-                      color: Colors.black,
+                      color: Colors.redAccent,
                     ),
                   ),
 
