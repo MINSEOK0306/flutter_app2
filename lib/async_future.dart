56c52ca6026d951ef49f6e4069aa252059d8b0d1
minseuk0306@naver.com <minseuk0306@naver.com>
2026-05-12 15:44:57 +0900

    260512 - 동기/비동기 연습_ get ~ then_ async ~ await_일반함수로

--- lib/async_future.dart
+++ lib/async_future.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'package:http/http.dart' as http;
 
 void first_work(){
   print("첫번째 작업");
@@ -26,19 +27,41 @@ Future<void> downloadGame(){
 Future<String> fetchusername(int id){
   return Future.delayed(
     Duration(seconds: 1), (){
-      return '사용자_달러_$id';
+      if(id <= 0) throw ArgumentError("잘못된 아이디입니다.");
+      return '사용자_$id';
   }
   );
 }
 
 //비동기
-void main(){
-  first_work();
-  //second_work();
-  // downloadGame();
-  fetchusername(1).then((name){
-    print('이름: $name');
-  // 시간이 걸릴 작업이오니 밑에꺼부터 실행햐라
-  });
-  third_work();
+void main() async{
+
+  // print("네이버 요청 시작");
+  //
+  // var username = await fetchusername(1);
+  // print(username);
+
+  // fetchusername(-1).then((name){
+  //   print('이름: $name');
+  //   // 시간이 걸릴 작업이오니 밑에꺼부터 실행햐라
+  // }).catchError((error){
+  //   print("오류: $error");
+  // });
+  ////-----------------------
+  // http.get(
+  //   Uri.parse("https://www.naver.com"),
+  // ).then((response){
+  //   print(response.body);
+  // });
+  //-----------------------------> get ~ then을 더 편하게 하기 위해 밑에 async(비동기) ~ var ~ await로 더 쉽게(then -> await)
+  var response = await http.get(
+    Uri.parse("https://www.naver.com"),
+  );
+  print(response.body);
+
+  // first_work();
+  // //second_work();
+  // // downloadGame();
+
+  // third_work();
 }
